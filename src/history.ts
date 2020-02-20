b5231f61cd2bc4920f064426c5f72ea4cec155f5
stefanos athanasoulias <stefanos.athanasoulas@ncryptedcloud.com>
2020-02-20 20:06:33 +0200

    Initial commit for moviesfinder project

---
+++ src/history.ts
@@ -0,0 +1,8 @@
+import * as history from 'history';
+import { getBasenamePath } from './utils/getBasenamePath';
+
+export default history.createBrowserHistory(
+    {
+        basename: getBasenamePath()
+    }
+);
---
+++ src/utils/getBasenamePath.ts
@@ -0,0 +1,12 @@
+
+import _get from 'lodash-es/get';
+
+const backend_constants = _get(window, 'nccDecryptProvider');
+
+export const getBasenamePath = (): string => {
+    const basename = backend_constants.isGuest
+        ? `/moviesfinder/`
+        : "/moviesfinder/login";
+
+    return basename;
+}
